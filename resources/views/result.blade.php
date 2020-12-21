0e093c1059982733397b0719dc10b77b1b0c3411
天野岳 <no13masterpiece@gmail.com>
2020-12-22 02:29:07 +0900

    aaaaaaaaaa

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -3,9 +3,9 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="app.css" >
-    <link rel="stylesheet" href="common.css">
-    <link rel="stylesheet" href="result.css">
+    <link href="{{asset('/css/app.css')}}" rel="stylesheet">
+    <link href="{{asset('/css/common.css')}}" rel="stylesheet">
+    <link href="{{asset('/css/result.css')}}" rel="stylesheet">
 
     <title>購入後画面</title>
   </head>
